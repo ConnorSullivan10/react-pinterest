cddfceeab146ee61cf16b1b2d8357567a5bb6161
Connor Sullivan <cksullvn@gmail.com>
2019-12-14 13:26:11 -0600

    works finally

--- src/App/App.js
+++ src/App/App.js
@@ -33,8 +33,20 @@ class App extends React.Component {
     this.setState({ selectedBoardId });
   }
 
-  render() {
+  renderView = () => {
     const { authed, selectedBoardId } = this.state;
+    if (!authed) {
+      return (<Auth />);
+    }
+
+    if (!selectedBoardId) {
+      return (<BoardsContainer setSingleBoard={this.setSingleBoard} />);
+    }
+    return (<SingleBoard selectedBoardId={selectedBoardId} setSingleBoard={this.setSingleBoard} />);
+  }
+
+  render() {
+    const { authed } = this.state;
 
     return (
       <div className="App">
@@ -42,12 +54,7 @@ class App extends React.Component {
           <button className="btn btn-success">Pinterest</button>
           {/* if they are authenticated, load the board */}
           {/* else show login button */}
-          {
-            (authed) ? (<BoardsContainer setSingleBoard={this.setSingleBoard}/>) : (<Auth />)
-          }
-          {
-            (selectedBoardId) && (<SingleBoard selectedBoardId={selectedBoardId} setSingleBoard={this.setSingleBoard}/>)
-          }
+            { this.renderView() }
       </div>
     );
   }
