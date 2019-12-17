b34e55e6477f39a765eb5e8bfe10437df98ec522
Connor Sullivan <cksullvn@gmail.com>
2019-12-16 20:41:45 -0600

    add board works

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Board from '../Board/Board';
 import authData from '../../helpers/data/authData';
 import boardData from '../../helpers/data/boardData';
+import BoardForm from '../BoardForm/BoardForm';
 
 class BoardsContainer extends React.Component {
   static propTypes = {
@@ -14,6 +15,10 @@ state ={
 }
 
 componentDidMount() {
+  this.getBoards();
+}
+
+getBoards = () => {
   boardData.getBoardsByUid(authData.getUid())
     .then((boards) => {
       this.setState({ boards });
@@ -21,9 +26,19 @@ componentDidMount() {
     .catch((errFromBoardsContainer) => console.error({ errFromBoardsContainer }));
 }
 
+
+addBoard = (newBoard) => {
+  boardData.saveBoard(newBoard)
+    .then(() => {
+      this.getBoards();
+    })
+    .catch((errorFromSaveBoard) => console.error({ errorFromSaveBoard }));
+}
+
 render() {
   const { setSingleBoard } = this.props;
   return (<div>
+    <BoardForm addBoard={this.addBoard}/>
     {this.state.boards.map((board) => (<Board key={board.id} board={board} setSingleBoard={setSingleBoard} />))}
   </div>);
 }
